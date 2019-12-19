b32a6b4b4f07645d9a2b9641485f351934d509f3
lyjzty6 <lyjzty6@163.com>
2019-12-19 11:05:59 +0800

    xixi

--- src/main/java/cn/zty/demo/controller/AuthorizeController.java
+++ src/main/java/cn/zty/demo/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -24,7 +26,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request
     ) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
@@ -35,9 +38,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getId());
-        System.out.println(user.getName());
-
-        return "index";
+        if (user !=null){
+            //登录成功
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            //登录失败
+            return "redirect:/";
+        }
     }
 }
