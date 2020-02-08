af5781b682c6c7049f19ebc6f698aad6978b63c8
lyjzty6 <lyjzty6@163.com>
2020-02-08 19:03:06 +0800

    g

--- src/test/java/cn/zty/demo/DemoApplicationTests.java
+++ src/test/java/cn/zty/demo/DemoApplicationTests.java
@@ -1,17 +1,15 @@
 package cn.zty.demo;
 
-import cn.zty.demo.dto.PaginationDTO;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
-    void contextLoads() {
-
-
-    }
+    void contextLoads() {}
 
 }
